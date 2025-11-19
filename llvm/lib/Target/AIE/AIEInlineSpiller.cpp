e83ff6f498175b5d480b278bf013a6f514917030
Fabian Stuckmann <fabian.stuckmann@amd.com>
2025-11-19 04:07:57 -0700

    [AIEX] Add AIE Spiller

---
+++ llvm/lib/Target/AIE/AIEInlineSpiller.cpp
@@ -0,0 +1,330 @@
+//===- AIEInlineSpiller.cpp - Custom AIE Inline Spiller -------------------===//
+//
+// This file is licensed under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+// (c) Copyright 2025 Advanced Micro Devices, Inc. or its affiliates
+//
+//===----------------------------------------------------------------------===//
+//
+// This file implements the AIEInlineSpiller class, which provides AIE-specific
+// spilling strategies by wrapping the standard InlineSpiller.
+//
+//===----------------------------------------------------------------------===//
+
+#include "AIEInlineSpiller.h"
+#include "llvm/ADT/Statistic.h"
+#include "llvm/CodeGen/LiveInterval.h"
+#include "llvm/CodeGen/LiveIntervals.h"
+#include "llvm/CodeGen/LiveRangeEdit.h"
+#include "llvm/CodeGen/LiveStacks.h"
+#include "llvm/CodeGen/MachineBasicBlock.h"
+#include "llvm/CodeGen/MachineFunction.h"
+#include "llvm/CodeGen/MachineInstr.h"
+#include "llvm/CodeGen/MachineInstrBuilder.h"
+#include "llvm/CodeGen/Spiller.h"
+#include "llvm/CodeGen/StackMaps.h"
+#include "llvm/CodeGen/TargetOpcodes.h"
+#include "llvm/CodeGen/VirtRegMap.h"
+#include "llvm/Support/Debug.h"
+
+using namespace llvm;
+
+#define DEBUG_TYPE "regalloc"
+
+STATISTIC(NumSubRegSpills, "Number of subregister spills inserted");
+STATISTIC(NumSubRegReloads, "Number of subregister reloads inserted");
+
+AIEInlineSpiller::AIEInlineSpiller(const Spiller::RequiredAnalyses &Analyses,
+                                   MachineFunction &MF, VirtRegMap &VRM,
+                                   VirtRegAuxInfo &VRAI)
+    : InlineSpiller(Analyses, MF, VRM, VRAI) {
+  // All common members initialized by InlineSpiller base constructor
+  // SpillInfos will be default-initialized (empty vector)
+  // Add AIE-specific initialization here if needed
+}
+
+void AIEInlineSpiller::spillAll() {
+  SpillInfo SI = collectSpillInfo();
+
+  SI.calcStack(MRI, TRI, VRM, LSS);
+
+  // todo: FIXME: perform optimizations
+
+  LLVM_DEBUG(SI.dump()); // MRI will be auto-fetched from
+                         // SpillLocations/ReloadLocations
+
+  SI.insertSpills(MRI, TII, TRI, VRM, LIS);
+  SI.insertReloads(MRI, TII, TRI, VRM, LIS);
+  SpillInfos.push_back(SI);
+  RegsToSpill.emplace_back(SI.getReg());
+  LLVM_DEBUG(MF.dump());
+}
+
+SpillInfo AIEInlineSpiller::collectSpillInfo() const {
+  SpillInfo SI(Original);
+  for (Register Reg : RegsToSpill) {
+    SI.update(Reg, MRI);
+  }
+  return SI;
+}
+
+void SpillInfo::calcStack(MachineRegisterInfo &MRI,
+                          const TargetRegisterInfo &TRI, VirtRegMap &VRM,
+                          LiveStacks &LSS) {
+
+  for (auto DefOp : DefOps) {
+    // get Original register from the defining operand
+    const TargetRegisterClass *RC = MRI.getRegClass(DefOp.getReg());
+    if (DefOp.getSubReg()) {
+      RC = TRI.getSubClassWithSubReg(RC, DefOp.getSubReg());
+    }
+
+    StackSlots.push_back(VRM.createSpillSlot(RC));
+  }
+}
+
+void SpillInfo::updateVRegOps(
+    ArrayRef<std::pair<MachineInstr *, unsigned>> Ops) {
+  // Save Operands of Register in this->Ops
+  this->Ops.clear();
+  this->Ops.append(Ops.begin(), Ops.end());
+
+  // Update DefOps with write definitions from the given operands.
+  for (const auto &Op : Ops) {
+    MachineOperand &MO = Op.first->getOperand(Op.second);
+    if (MO.isDef()) {
+      // Use isIdenticalTo for comparison instead of is_contained, since
+      // MachineOperand does not have operator== defined for direct container
+      // search.
+      bool Found = false;
+      for (const auto &DefOp : DefOps) {
+        if (DefOp.isIdenticalTo(MO)) {
+          Found = true;
+          break;
+        }
+      }
+      if (!Found) {
+        LLVM_DEBUG(dbgs() << "Adding write def: " << MO << '\n');
+        DefOps.push_back(MO);
+      } else {
+        LLVM_DEBUG(dbgs() << "Write def already exists: " << MO << '\n');
+      }
+    }
+  }
+}
+
+void SpillInfo::update(Register Reg, MachineRegisterInfo &MRI) {
+  // Iterate over instructions using Reg.
+  for (MachineInstr &MI : llvm::make_early_inc_range(MRI.reg_bundles(Reg))) {
+    if (MI.isDebugValue()) {
+      LLVM_DEBUG(dbgs() << "Skipping debug value: " << MI);
+      continue;
+    }
+
+    // Analyze instruction.
+    SmallVector<std::pair<MachineInstr *, unsigned>, 8> Ops;
+    VirtRegInfo RI = AnalyzeVirtRegInBundle(MI, Reg, &Ops);
+
+    if (RI.Writes) {
+      // Save the defining operands of the write.
+      updateVRegOps(Ops);
+      LLVM_DEBUG(dbgs() << "Adding spill location: " << MI);
+      SpillLocations.push_back(&MI);
+    }
+
+    if (RI.Reads) {
+      LLVM_DEBUG(dbgs() << "Adding reload location: " << MI);
+      ReloadLocations.push_back(&MI);
+    }
+  }
+}
+
+void SpillInfo::insertSpill(MachineInstr *MI, bool IsKill,
+                            MachineRegisterInfo &MRI,
+                            const TargetInstrInfo &TII,
+                            const TargetRegisterInfo &TRI, VirtRegMap &VRM,
+                            LiveIntervals &LIS) {
+  MachineBasicBlock &MBB = *MI->getParent();
+  MachineInstrSpan MIS(MI, &MBB);
+  MachineBasicBlock::iterator SpillBefore = std::next(MI->getIterator());
+
+  for (unsigned I = 0; I < StackSlots.size(); I++) {
+    Register OrigReg = DefOps[I].getReg();
+    int StackSlot = StackSlots[I];
+    const TargetRegisterClass *RC = MRI.getRegClass(OrigReg);
+
+    const bool IsSubReg = DefOps[I].getSubReg() != 0;
+
+    // Create a new virtual register
+    Register NewVReg = MRI.createVirtualRegister(RC);
+    SpillVRegs.push_back(NewVReg);
+
+    // Create COPY: NewVReg = COPY OrigReg
+    BuildMI(MBB, SpillBefore, MI->getDebugLoc(), TII.get(TargetOpcode::COPY),
+            NewVReg)
+        .addReg(OrigReg, getKillRegState(IsKill));
+
+    // Assign the new virtual register to the stack
+    // slot
+    VRM.assignVirt2StackSlot(NewVReg, StackSlot);
+
+    // Store the new virtual register to the stack
+    // slot
+    TII.storeRegToStackSlot(MBB, SpillBefore, NewVReg, true, StackSlot, RC,
+                            &TRI, Register());
+
+    if (IsSubReg)
+      NumSubRegSpills++;
+    else
+      NumSpills++;
+  }
+
+  MachineBasicBlock::iterator Spill = std::next(MI->getIterator());
+  LIS.InsertMachineInstrRangeInMaps(Spill, MIS.end());
+}
+
+Register SpillInfo::insertReload(MachineInstr *MI, MachineRegisterInfo &MRI,
+                                 const TargetInstrInfo &TII,
+                                 const TargetRegisterInfo &TRI, VirtRegMap &VRM,
+                                 LiveIntervals &LIS) {
+  MachineBasicBlock &MBB = *MI->getParent();
+  MachineInstrSpan MIS(MI, &MBB);
+
+  // Create a new virtual register
+  const TargetRegisterClass *RC = MRI.getRegClass(Reg);
+  Register NewVReg = MRI.createVirtualRegister(RC);
+
+  for (unsigned I = 0; I < StackSlots.size(); I++) {
+    Register OrigReg = DefOps[I].getReg();
+    unsigned SubRegIdx = DefOps[I].getSubReg();
+    int StackSlot = StackSlots[I];
+
+    const bool IsSubReg = DefOps[I].getSubReg() != 0;
+    unsigned AdditionalFlag = IsSubReg && I == 0 ? getUndefRegState(true) : 0;
+
+    // Assign the new virtual register to the stack slot
+    const TargetRegisterClass *SubRC = MRI.getRegClass(OrigReg);
+    Register TempReg = MRI.createVirtualRegister(SubRC);
+    VRM.assignVirt2StackSlot(TempReg, StackSlot);
+
+    TII.loadRegFromStackSlot(MBB, MI, TempReg, StackSlot, SubRC, &TRI,
+                             Register());
+
+    // Copy from the temporary to the parent register's subregister
+    BuildMI(MBB, MI, MI->getDebugLoc(), TII.get(TargetOpcode::COPY))
+        .addReg(NewVReg, RegState::Define | AdditionalFlag, SubRegIdx)
+        .addReg(TempReg, RegState::Kill);
+
+    if (IsSubReg)
+      NumSubRegReloads++;
+    else
+      NumReloads++;
+  }
+
+  LIS.InsertMachineInstrRangeInMaps(MIS.begin(), MI);
+  return NewVReg;
+}
+
+void SpillInfo::replaceVReg(Register NewVReg) {
+  // Todo: will i not replace Uses before spilling?
+  for (const auto &OpPair : Ops) {
+    MachineOperand &MO = OpPair.first->getOperand(OpPair.second);
+    LLVM_DEBUG(dbgs() << "Replacing virtual register: " << printReg(MO.getReg())
+                      << " in " << *OpPair.first << " with "
+                      << printReg(NewVReg) << "\n";);
+    MO.setReg(NewVReg);
+    if (!MO.isUse())
+      continue;
+
+    if (!OpPair.first->isRegTiedToDefOperand(OpPair.second))
+      MO.setIsKill();
+  }
+}
+
+void SpillInfo::insertSpills(MachineRegisterInfo &MRI,
+                             const TargetInstrInfo &TII,
+                             const TargetRegisterInfo &TRI, VirtRegMap &VRM,
+                             LiveIntervals &LIS) {
+  const bool IsKill = true;
+  for (MachineInstr *MI : SpillLocations) {
+    insertSpill(MI, IsKill, MRI, TII, TRI, VRM, LIS);
+  }
+}
+
+void SpillInfo::insertReloads(MachineRegisterInfo &MRI,
+                              const TargetInstrInfo &TII,
+                              const TargetRegisterInfo &TRI, VirtRegMap &VRM,
+                              LiveIntervals &LIS) {
+  for (MachineInstr *MI : ReloadLocations) {
+    Register NewVReg = insertReload(MI, MRI, TII, TRI, VRM, LIS);
+    replaceVReg(NewVReg);
+  }
+}
+
+void SpillInfo::dump() const {
+  // Lambda to get MRI from any available instruction pointer
+  auto GetMRI = [this]() -> const MachineRegisterInfo * {
+    const MachineInstr *MI = nullptr;
+    if (!SpillLocations.empty())
+      MI = SpillLocations[0];
+    else if (!ReloadLocations.empty())
+      MI = ReloadLocations[0];
+
+    if (MI)
+      if (const MachineFunction *MF = MI->getMF())
+        return &MF->getRegInfo();
+    return nullptr;
+  };
+
+  const MachineRegisterInfo *MRI = GetMRI();
+  const TargetRegisterInfo *TRI = MRI ? MRI->getTargetRegisterInfo() : nullptr;
+
+  dbgs() << "SpillInfo for register: " << printReg(Reg);
+  if (MRI && Reg.isVirtual()) {
+    const TargetRegisterClass *RC = MRI->getRegClass(Reg);
+    dbgs() << ", RC: " << TRI->getRegClassName(RC);
+  }
+  dbgs() << "\n";
+
+  dbgs() << "  Ops (" << Ops.size() << "):\n";
+  for (const auto &Op : Ops) {
+    dbgs() << "    " << printReg(Op.second) << " " << *Op.first << "\n";
+  }
+
+  dbgs() << "  DefOps (" << DefOps.size() << "):\n";
+  for (const auto &MO : DefOps) {
+    dbgs() << "    Reg: " << printReg(MO.getReg());
+    if (MRI && MO.getReg().isVirtual()) {
+      const TargetRegisterClass *RC = MRI->getRegClass(MO.getReg());
+      dbgs() << ", RC: " << TRI->getRegClassName(RC);
+    }
+    dbgs() << ", SubReg: " << MO.getSubReg() << "\n";
+  }
+
+  dbgs() << "  StackSlots (" << StackSlots.size() << "):\n";
+  for (auto StackSlot : StackSlots) {
+    dbgs() << "    StackSlot: " << StackSlot << "\n";
+  }
+
+  dbgs() << "  SpillVRegs (" << SpillVRegs.size() << "):\n";
+  for (auto VReg : SpillVRegs) {
+    dbgs() << "    " << printReg(VReg);
+    if (MRI && VReg.isVirtual()) {
+      const TargetRegisterClass *RC = MRI->getRegClass(VReg);
+      dbgs() << ", RC: " << TRI->getRegClassName(RC);
+    }
+    dbgs() << "\n";
+  }
+
+  dbgs() << "  SpillLocations (" << SpillLocations.size() << "):\n";
+  for (const auto *MI : SpillLocations) {
+    dbgs() << "    " << *MI;
+  }
+
+  dbgs() << "  ReloadLocations (" << ReloadLocations.size() << "):\n";
+  for (const auto *MI : ReloadLocations) {
+    dbgs() << "    " << *MI;
+  }
+}
---
+++ llvm/lib/Target/AIE/AIEInlineSpiller.h
@@ -0,0 +1,233 @@
+//===- AIEInlineSpiller.h - Custom AIE Inline Spiller -----------*- C++ -*-===//
+//
+// This file is licensed under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+// (c) Copyright 2025 Advanced Micro Devices, Inc. or its affiliates
+//
+//===----------------------------------------------------------------------===//
+//
+// Custom AIE inline spiller.
+//
+//===----------------------------------------------------------------------===//
+
+#ifndef LLVM_LIB_TARGET_AIE_AIEINLINESPILLER_H
+#define LLVM_LIB_TARGET_AIE_AIEINLINESPILLER_H
+
+#include "llvm/CodeGen/InlineSpiller.h"
+#include "llvm/CodeGen/MachineOperand.h"
+#include "llvm/CodeGen/TargetInstrInfo.h"
+#include <memory>
+
+namespace llvm {
+
+/// SpillInfo - Encapsulates all information needed to spill a single register.
+///
+/// This class tracks the original register being spilled, its defining
+/// operands, the stack slots allocated for spilling, and the locations where
+/// spill stores and reload loads should be inserted. It provides methods to
+/// calculate stack slot assignments and insert the actual spill/reload
+/// instructions.
+///
+/// The typical usage pattern is:
+/// 1. Create a SpillInfo for a register
+/// 2. Call update() to collect defining operands and spill/reload locations
+/// 3. Call calcStack() to allocate stack slots for each defining operand
+/// 4. Call insertSpills() and insertReloads() to insert the actual instructions
+class SpillInfo {
+  /// Original register that is being spilled.
+  Register Reg;
+
+  SmallVector<std::pair<MachineInstr *, unsigned>, 8> Ops;
+
+  /// Defining operands of the original register.
+  /// Each defining operand may require a separate stack slot if it defines
+  /// a subregister or has different register class requirements.
+  SmallVector<MachineOperand, 8> DefOps;
+
+  /// Stack slots used to spill the original register.
+  /// One stack slot is allocated for each defining operand in DefOps.
+  SmallVector<unsigned, 8> StackSlots;
+
+  /// LiveInterval objects for the stack slots.
+  SmallVector<LiveInterval *, 8> StackInts;
+
+  /// New virtual registers created for spilling.
+  /// These temporary registers are used to transfer values between the
+  /// original register and the stack slots.
+  SmallVector<Register, 8> SpillVRegs;
+
+  /// Instructions after which to insert spill stores.
+  /// Spill stores are inserted immediately after the instruction that
+  /// defines the value being spilled.
+  SmallVector<MachineInstr *, 8> SpillLocations;
+
+  /// Instructions before which to insert reload loads.
+  /// Reload loads are inserted immediately before the instruction that
+  /// uses the spilled value.
+  SmallVector<MachineInstr *, 8> ReloadLocations;
+
+  /// Insert a spill store instruction after the given instruction.
+  /// Creates a COPY from the original register to a new virtual register,
+  /// then stores that virtual register to the appropriate stack slot.
+  ///
+  /// \param MI Iterator pointing to the instruction after which to insert
+  /// \param IsKill Whether the source register is killed by this spill
+  /// \param MRI Machine register info
+  /// \param TII Target instruction info
+  /// \param TRI Target register info
+  /// \param VRM Virtual register map
+  /// \param LIS Live intervals
+  void insertSpill(MachineInstr *MI, bool IsKill, MachineRegisterInfo &MRI,
+                   const TargetInstrInfo &TII, const TargetRegisterInfo &TRI,
+                   VirtRegMap &VRM, LiveIntervals &LIS);
+
+  /// Insert a reload load instruction before the given instruction.
+  /// Loads from stack slots into temporary virtual registers, then copies
+  /// those values to subregisters of a new parent register.
+  ///
+  /// \param MI Instruction before which to insert the reload
+  /// \param MRI Machine register info
+  /// \param TII Target instruction info
+  /// \param TRI Target register info
+  /// \param VRM Virtual register map
+  /// \param LIS Live intervals
+  Register insertReload(MachineInstr *MI, MachineRegisterInfo &MRI,
+                        const TargetInstrInfo &TII,
+                        const TargetRegisterInfo &TRI, VirtRegMap &VRM,
+                        LiveIntervals &LIS);
+
+  void updateVRegOps(ArrayRef<std::pair<MachineInstr *, unsigned>> Ops);
+
+  /// Replace the virtual register in the operands with the given new virtual
+  /// register.
+  ///
+  /// \param NewVReg The new virtual register to replace the old one
+  void replaceVReg(Register NewVReg);
+
+public:
+  /// Constructor - Initialize SpillInfo for the given register.
+  ///
+  /// \param Reg The original register to be spilled
+  SpillInfo(Register Reg) : Reg(Reg) {}
+
+  /// Collect spill and reload information for the given register.
+  /// Analyzes all instructions using the register to identify write definitions
+  /// (spill locations) and read uses (reload locations).
+  ///
+  /// \param Reg The register to analyze
+  /// \param MRI Machine register info
+  void update(const Register Reg, MachineRegisterInfo &MRI);
+
+  /// Calculate and allocate stack slots for spilling.
+  /// Creates one stack slot for each defining operand, taking into account
+  /// register classes and subregister indices.
+  ///
+  /// \param MRI Machine register info
+  /// \param TRI Target register info
+  /// \param VRM Virtual register map
+  /// \param LSS Live stacks
+  void calcStack(MachineRegisterInfo &MRI, const TargetRegisterInfo &TRI,
+                 VirtRegMap &VRM, LiveStacks &LSS);
+
+  /// Insert all spill store instructions at the collected spill locations.
+  /// For each spill location, creates a COPY and store instruction to save
+  /// the register value to the stack.
+  ///
+  /// \param MRI Machine register info
+  /// \param TII Target instruction info
+  /// \param TRI Target register info
+  /// \param VRM Virtual register map
+  /// \param LIS Live intervals
+  void insertSpills(MachineRegisterInfo &MRI, const TargetInstrInfo &TII,
+                    const TargetRegisterInfo &TRI, VirtRegMap &VRM,
+                    LiveIntervals &LIS);
+
+  /// Insert all reload load instructions at the collected reload locations.
+  /// For each reload location, creates load and COPY instructions to restore
+  /// the register value from the stack.
+  ///
+  /// \param MRI Machine register info
+  /// \param TII Target instruction info
+  /// \param TRI Target register info
+  /// \param VRM Virtual register map
+  /// \param LIS Live intervals
+  void insertReloads(MachineRegisterInfo &MRI, const TargetInstrInfo &TII,
+                     const TargetRegisterInfo &TRI, VirtRegMap &VRM,
+                     LiveIntervals &LIS);
+
+  /// Get the original register being spilled.
+  ///
+  /// \return The original register
+  Register getReg() const { return Reg; }
+
+  /// Dump the SpillInfo for debugging purposes.
+  /// Prints the register, defining operands, stack slots, spill virtual
+  /// registers, and spill/reload locations.
+  void dump() const;
+};
+
+/// AIEInlineSpiller - AIE-specific register spiller.
+///
+/// This class extends the base Spiller functionality to provide AIE-specific
+/// spilling strategies. It handles the spilling of virtual registers to stack
+/// slots, including special handling for "snippets" - small live ranges that
+/// are created by live range splitting and can be profitably spilled to
+/// tighten register pressure.
+///
+/// Key features:
+/// - Identifies and spills snippet registers alongside the main register
+/// - Creates SpillInfo objects to track all spill/reload operations
+/// - Integrates with LiveIntervals to maintain accurate liveness information
+/// - Works with VirtRegMap to manage stack slot assignments
+///
+/// The spilling process:
+/// 1. collectRegsToSpill() - Identify all registers (including snippets) to
+/// spill
+/// 2. collectSpillInfo() - Gather information about spill/reload locations
+/// 3. spillAll() - Allocate stack slots and insert spill/reload instructions
+class AIEInlineSpiller : public InlineSpiller {
+  /// Collection of SpillInfo objects created during spilling.
+  /// Each SpillInfo tracks the spill/reload operations for one register.
+  SmallVector<SpillInfo, 8> SpillInfos;
+
+public:
+  /// Constructor - Initialize the AIE inline spiller.
+  ///
+  /// \param Analyses Required analyses including LiveIntervals and LiveStacks
+  /// \param MF Machine function being processed
+  /// \param VRM Virtual register map
+  /// \param VRAI Virtual register auxiliary info for weight calculation
+  AIEInlineSpiller(const Spiller::RequiredAnalyses &Analyses,
+                   MachineFunction &MF, VirtRegMap &VRM, VirtRegAuxInfo &VRAI);
+
+protected:
+  /// Perform the actual spilling of all collected registers.
+  /// Creates a SpillInfo object, calculates stack slot assignments, and
+  /// inserts all spill/reload instructions.
+  void spillAll() override;
+
+  /// Collect spill information for all registers in RegsToSpill.
+  /// Creates a SpillInfo object and populates it with defining operands,
+  /// spill locations, and reload locations for all registers being spilled.
+  ///
+  /// \return SpillInfo containing all collected information
+  SpillInfo collectSpillInfo() const;
+
+  /// Insert spill store instructions for the given SpillInfo.
+  /// Wrapper method that delegates to SpillInfo::insertSpills().
+  ///
+  /// \param SI SpillInfo describing where to insert spills
+  void insertSpills(SpillInfo &SI);
+
+  /// Insert reload load instructions for the given SpillInfo.
+  /// Wrapper method that delegates to SpillInfo::insertReloads().
+  ///
+  /// \param SI SpillInfo describing where to insert reloads
+  void insertReloads(SpillInfo &SI);
+};
+
+} // namespace llvm
+
+#endif // LLVM_LIB_TARGET_AIE_AIEINLINESPILLER_H
